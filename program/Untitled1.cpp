6f6cbe4f152c6257cb7fb160598c690512ac5852
AT043 <daffa.assyam.thariq.an21@mhsw.pnj.ac.id>
2023-01-02 22:03:50 +0700

    fproject1.22

--- program/Untitled1.cpp
+++ program/Untitled1.cpp
@@ -11,7 +11,7 @@ struct identity {
 struct tiket {
 	int hargaTiket[1001] = {30000, 40000, 20000, 20000, 35000};
 	string nominal[1001] = {"Rp 30.000", "Rp 40.000", "Rp 20.000", "Rp 20.000", "Rp 35.000"};
-	int tglBrkt[1001] = {1,2, 3, 4, 5, 6, 7, 8, 9, 10};
+	int tglBrkt[1001] = {1,2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31};
 	int bulan[1001] = {1, 2, 3, 4, 5, 6, 7 , 8, 9, 10, 11, 12};
 	int tahun = 2005;
 	string namaKa[1001] = {"Sriwijaya", "Rajabasa", "Prabu Jaya", "Serelo", "Sindang Marga"};
@@ -19,8 +19,10 @@ struct tiket {
 };
 
 struct jadwal {
-    string tujuan[1001] = {"Tanjungkarang(TNK)", "Tanjungkarang(TNK)", "Prabumulih(PBM)", "Lubuklinggau(LLG)", "Lubuklinggau(LLG)"};
-    string waktu[1001] = {"10.00", "20.00", "12.15", "14.00", "16.30"};
+    string tujuanPergi[1001] = {"Kertapati(KPT)", "Kertapati(KPT)", "Prabumulih(PBM)", "Lubuklinggau(LLG)", "Lubuklinggau(LLG)"};
+    string tujuanPulang[1001] = {"Tanjungkarang(TNK)", "Tanjungkarang(TNK)", "Kertapati(KPT)", "Kertapati(KPT)", "Kertapati(KPT)"};
+    string waktuPergi[1001] = {"10.00", "11.00", "12.15", "08.00", "08.30"};
+    string waktuPulang[1001] = {"16.00", "17.00", "16.30", "13.00", "14.00" };
     struct identity customer;
     struct tiket tiketNya;
 };
@@ -28,7 +30,7 @@ struct jadwal {
     
 void welcome() {
 	cout<<" Selamat datang di sistem pemesanan tiket kereta \n";
-	cout<<"        DIVRE III Sumatera Selatan     "<<endl;
+	cout<<"          DIVREG III Sumatera Selatan     "<<endl;
 	cout<<"=================================================="<<endl;
 	
 }
@@ -42,7 +44,7 @@ void menuAwal(int pilih) {
 
 void pilihStasiun(int choose) {
 	cout<<" Pilih Stasiun Keberangkatan: "<<endl;
-	cout<<"1. Tanjungkarang(TNK)"<<endl<<"2. Prabumulih(PBM)"<<endl<<"3.Lubuklinggau(LLG)\n";
+	cout<<"1. Tanjungkarang(TNK)"<<endl<<"2. Prabumulih(PBM)"<<endl<<"3. Lubuklinggau(LLG)"<<endl<<"4. Kertapati(KPT)";
 	cout<<endl;
 }
 
@@ -81,13 +83,27 @@ int main() {
 			
 				switch(menuA) {
 			case 1 :
-				cout<<"Infonecak"<<endl;
+				int selectA;
+				cout<<"Pesan tiket kereta ke: "<<endl;
+				cout<<"1."<<pesanTiket.tujuanPergi[2]<<endl;
+				cout<<"2."<<pesanTiket.tujuanPergi[4]<<endl;
+				cin>>selectA;
+					if(selectA==1) {
+						cout<<"Kereta tersedia: "<<endl;
+						cout<<pesanTiket.tiketNya.namaKa[1]<<"-"<<pesanTiket.tiketNya.jenisKereta[1]<<endl;
+						cout<<"jadwal: "<<pesanTiket.waktuPergi[1]<<endl;
+					} else if(selectA==2) {
+						cout<<"Kereta tersedia: "<<endl;
+					}
 				break;
 			case 2 :
-				cout<<"Jadwale rung diatur mazzsseh!"<<endl;
+				cout<<"Pesan tiket kereta ke:"<<endl;
 				break;
 			case 3 :
-				cout<<"Dudu info lokeran mazzsseh!"<<endl;
+				cout<<"Pesan tiket kereta ke: "<<endl;
+				break;
+			case 4 :
+				cout<<"Pesan tiket kereta ke: "<<endl;
 				break;
 			default: cout<<"Silahkan pilih sesuai yang ada di daftar!"<<endl;
 			}
@@ -97,12 +113,27 @@ int main() {
 		} else if(menu==2) {
 			for (int i=0; i<5; i++) {
 				cout<<" Nama dan No KA: "<< pesanTiket.tiketNya.namaKa[i] <<endl;
-				cout<<" Asal: Kertapati(KPT)" <<" Tujuan: "<<pesanTiket.tujuan[i]<<endl;
-				cout<<" Keberangkatan: "<<pesanTiket.waktu[i]<<endl;
+				cout<<" Asal: Kertapati(KPT)" <<" Tujuan: "<<pesanTiket.tujuanPergi[i]<<endl;
+				cout<<" Keberangkatan: "<<pesanTiket.waktuPergi[i]<<endl;
 				cout<<"\n";
 			};
 			
-			thanks();
+			for (int i=0; i<5; i++) {
+				cout<<" Nama dan No KA: "<< pesanTiket.tiketNya.namaKa[i] <<endl;
+				cout<<" Asal: Kertapati(KPT)" <<" Tujuan: "<<pesanTiket.tujuanPulang[i]<<endl;
+				cout<<" Keberangkatan: "<<pesanTiket.waktuPulang[i]<<endl;
+				cout<<"\n";
+			};
+			
+			int backButton;
+			cout<<"1. Kembali 2. Selesai"<<endl;
+			cin>>backButton;
+				if (backButton == 1) {
+					cout<<"Kembalilah"<<endl;
+				} else if (backButton == 2) {
+					thanks();
+				}
+			
 		} else if(menu==3) {
 			cout<<"Blong"<<endl;
 			thanks();
