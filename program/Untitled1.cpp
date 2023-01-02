3e1810dce3fd848de1490e18ace969aa43c4b9d6
AT043 <daffa.assyam.thariq.an21@mhsw.pnj.ac.id>
2023-01-02 14:08:15 +0700

    fproject1

--- program/Untitled1.cpp
+++ program/Untitled1.cpp
@@ -2,36 +2,41 @@
 
 using namespace std;
 
-struct pesanTiket {
+struct identity {
+	string nama;
+	int jumlahTiket;
+	long long noIdentitas;
+};
+
+struct tiket {
 	int hargaTiket[1001] = {30000, 40000, 20000, 20000, 35000};
-	int tglBrkt[1001] = {1, 1, 1, 1, 2};
-	int bulan[1001] = {1, 1, 1, 1, 1};
+	int tglBrkt[1001] = {1,2};
+	int bulan[1001] = {1};
 	int tahun[2] = {2015};
 	string nomorKa[1001] = {"PA-302", "TT-333", "ZZ-201", "NT-203", "PA-304"};
 };
 
-struct lihatJadwal {
+/*struct jadwal {
     string tujuan[1001] = {"Surabaya", "Jakarta", "Solo", "Yogya", "Malang"};
     string stasiun[1001] = {"Stasiun Gubeng", "Stasiun Gambir", "Stasiun Balapan", "Stasiun diYogya", "Stasiun diMalang"};
     string 
     double waktu[1001] = {10.19, 20.00, 2.00};
-    struct pesanTiket tiketNya;
+    struct tiket tiketNya;
 };
-    kereta tiket;
+    kereta tiket;*/
     
-void welcome(char wcomeGreet[1001]) {
-	cout<<wcomeGreet<<endl;
-	cout<<"=================================="<<endl;
+void welcome() {
+	cout<<" Selamat datang di sistem pemesanan tiket kereta \n";
+	cout<<"           Stasiun 4379 Rengasdengklok      "<<endl;
+	cout<<"=================================================="<<endl;
 	
 }
 
 void menuAwal(int pilih) {
-	cout<<"  Apa yang Anda ingin lakukan?  "<<endl;
+	cout<<"  Apa yang Anda cari?  "<<endl;
 	cout<<"1. Pesan Tiket"<<endl<<"2. Lihat Jadwal"<<endl<<"3. Info Lainnya"<<endl;
-	system("pause");
-	system("cls");
 	cout<<endl;
-	switch(pilih) {
+	/*switch(pilih) {
 		case 1 :
 			cout<<"Infonecak"<<endl;
 			break;
@@ -43,7 +48,8 @@ void menuAwal(int pilih) {
 			break;
 		default: cout<<"Silahkan pilih sesuai yang ada di daftar!"<<endl;
 		
-	}
+	}*/
+	
 }
 
 void thanks() {
@@ -52,11 +58,38 @@ void thanks() {
 	
 }
 
+int time() {
+	// declaring argument of time()
+	time_t my_time = time(NULL);
+
+	// ctime() used to give the present time
+	printf("waktu pesan: %s", ctime(&my_time));
+	return 0;
+}
+
 
 
 int main() {
-	welcome("  Selamat datang di Stasiun Waru  ");
-	menuAwal(2);
-	thanks();
+	welcome();
 	
+	int menu;
+	menuAwal(menu);
+	cin>>menu;
+	system("cls");
+	
+		if (menu==1) {
+			cout<<"Pilih Tujuan: "<<endl;
+			time();
+			thanks();
+		} else if(menu==2) {
+			cout<<"Nama dan No KA: TT-303 Ka Wilis Tujuan: xx Keberangkatan: 00"<<endl;
+			thanks();
+		} else if(menu==3) {
+			cout<<"Blong"<<endl;
+			thanks();
+		} else {
+			cout<<"Masukkan Anda tidak dikenal, tolong pilih sesuai angka 1 - 3 sesuai di daftar!"<<endl;
+		}
+	
+	return 0;
 }
