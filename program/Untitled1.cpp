2ffec0855f010fd5491f87a6bf59a78bbbf3886d
AT043 <daffa.assyam.thariq.an21@mhsw.pnj.ac.id>
2023-01-03 08:03:21 +0700

    fproject1.23

--- program/Untitled1.cpp
+++ program/Untitled1.cpp
@@ -2,33 +2,34 @@
 
 using namespace std;
 
-struct identity {
+struct passanger {
 	string nama;
 	int jumlahTiket;
 	long long noIdentitas;
 };
 
 struct tiket {
-	int hargaTiket[1001] = {30000, 40000, 20000, 20000, 35000};
-	string nominal[1001] = {"Rp 30.000", "Rp 40.000", "Rp 20.000", "Rp 20.000", "Rp 35.000"};
+	int hargaTiket[1001] = {50000, 35000, 30000, 30000, 35000};
+	//string nominal[1001] = {"Rp 50.000", "Rp 35.000", "Rp 30.000", "Rp 30.000", "Rp 35.000"};
 	int tglBrkt[1001] = {1,2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31};
 	int bulan[1001] = {1, 2, 3, 4, 5, 6, 7 , 8, 9, 10, 11, 12};
-	int tahun = 2005;
+	int tahun = 2023;
 	string namaKa[1001] = {"Sriwijaya", "Rajabasa", "Prabu Jaya", "Serelo", "Sindang Marga"};
 	string jenisKereta[1001] = {"Eksekutif Premium", "Ekonomi AC", "Eksekutif Bisnis", "Ekonomi AC", "Eksekutif Bisnis"};
 };
 
 struct jadwal {
-    string tujuanPergi[1001] = {"Kertapati(KPT)", "Kertapati(KPT)", "Prabumulih(PBM)", "Lubuklinggau(LLG)", "Lubuklinggau(LLG)"};
-    string tujuanPulang[1001] = {"Tanjungkarang(TNK)", "Tanjungkarang(TNK)", "Kertapati(KPT)", "Kertapati(KPT)", "Kertapati(KPT)"};
-    string waktuPergi[1001] = {"10.00", "11.00", "12.15", "08.00", "08.30"};
-    string waktuPulang[1001] = {"16.00", "17.00", "16.30", "13.00", "14.00" };
-    struct identity customer;
+    string tujuanPergi[1001] = {"Kertapati(KPT)", "Tanjungkarang(TNK)", "Prabumulih(PBM)", "Lubuklinggau(LLG)", "Kertapati(KPT)"};
+    string tujuanPulang[1001] = {"Tanjungkarang(TNK)", "Kertapati(KPT)", "Kertapati(KPT)", "Kertapati(KPT)", "Lubuklinggau(LLG)"};
+    string waktuPergi[1001] = {"10.00", "08.30", "12.15", "08.00", "19.45"};
+    string waktuSampai[1001] = {"17.30", "17.30", "16.30", "13.00", "02.25" };
+    struct passanger identity;
     struct tiket tiketNya;
 };
     jadwal pesanTiket;
     
 void welcome() {
+	cout<<"=================================================="<<endl;
 	cout<<" Selamat datang di sistem pemesanan tiket kereta \n";
 	cout<<"          DIVREG III Sumatera Selatan     "<<endl;
 	cout<<"=================================================="<<endl;
@@ -44,7 +45,7 @@ void menuAwal(int pilih) {
 
 void pilihStasiun(int choose) {
 	cout<<" Pilih Stasiun Keberangkatan: "<<endl;
-	cout<<"1. Tanjungkarang(TNK)"<<endl<<"2. Prabumulih(PBM)"<<endl<<"3. Lubuklinggau(LLG)"<<endl<<"4. Kertapati(KPT)";
+	cout<<"1. Kertapati(KPT)"<<endl<<"2. Prabumulih(PBM)"<<endl<<"3. Lubuklinggau(LLG)"<<endl<<"4. Tanjungkarang(TNK)";
 	cout<<endl;
 }
 
@@ -67,7 +68,7 @@ int time() {
 
 int main() {
 	
-	welcome();
+	welcome(); 
 	
 	int menu;
 	menuAwal(menu);
@@ -75,55 +76,169 @@ int main() {
 	system("cls");
 	
 	
-	
+	welcome();
 		if (menu==1) {
 			int menuA;
 			pilihStasiun(menuA);
+			cout<<"=================================================="<<endl;
 			cin>>menuA;
+			system("cls");
 			
+				welcome();
 				switch(menuA) {
 			case 1 :
 				int selectA;
 				cout<<"Pesan tiket kereta ke: "<<endl;
-				cout<<"1."<<pesanTiket.tujuanPergi[2]<<endl;
-				cout<<"2."<<pesanTiket.tujuanPergi[4]<<endl;
+				cout<<"1."<<pesanTiket.tujuanPergi[1]<<endl;
+				cout<<"2."<<pesanTiket.tujuanPergi[2]<<endl;
+				cout<<"3."<<pesanTiket.tujuanPergi[3]<<endl;
+				cout<<"=================================================="<<endl;
 				cin>>selectA;
+				system("cls");
 					if(selectA==1) {
 						cout<<"Kereta tersedia: "<<endl;
 						cout<<pesanTiket.tiketNya.namaKa[1]<<"-"<<pesanTiket.tiketNya.jenisKereta[1]<<endl;
-						cout<<"jadwal: "<<pesanTiket.waktuPergi[1]<<endl;
+						cout<<" Tanggal keberangkatan: "<<pesanTiket.tiketNya.tglBrkt[2]<<"-"<<pesanTiket.tiketNya.bulan[1]<<"-"<<pesanTiket.tiketNya.tahun<<endl;
+						cout<<" Waktu Keberangkatan: "<<pesanTiket.waktuPergi[1]<<endl;
+						cout<<endl;
+						cout<<"Pesan Tiket?"<<endl;
 					} else if(selectA==2) {
 						cout<<"Kereta tersedia: "<<endl;
+						cout<<pesanTiket.tiketNya.namaKa[2]<<"-"<<pesanTiket.tiketNya.jenisKereta[2]<<endl;
+						cout<<" Tanggal keberangkatan: "<<pesanTiket.tiketNya.tglBrkt[2]<<"-"<<pesanTiket.tiketNya.bulan[1]<<"-"<<pesanTiket.tiketNya.tahun<<endl;
+						cout<<" Waktu Keberangkatan: "<<pesanTiket.waktuPergi[2]<<endl;
+						cout<<endl;
+						cout<<"Pesan Tiket?"<<endl;
+					} else if(selectA==3) {
+						cout<<"Kereta tersedia: "<<endl;
+						cout<<pesanTiket.tiketNya.namaKa[3]<<"-"<<pesanTiket.tiketNya.jenisKereta[3]<<endl;
+						cout<<" Tanggal keberangkatan: "<<pesanTiket.tiketNya.tglBrkt[2]<<"-"<<pesanTiket.tiketNya.bulan[1]<<"-"<<pesanTiket.tiketNya.tahun<<endl;
+						cout<<" Waktu Keberangkatan: "<<pesanTiket.waktuPergi[3]<<endl;
+						cout<<endl;
+						cout<<"=================================================="<<endl;
+						cout<<"Pesan Tiket?"<<endl;
 					}
 				break;
 			case 2 :
-				cout<<"Pesan tiket kereta ke:"<<endl;
+				int selectB;
+				cout<<"Pesan tiket kereta ke: "<<endl;
+				cout<<"1."<<pesanTiket.tujuanPergi[2]<<endl;
+				cout<<"=================================================="<<endl;
+				cin>>selectA;
+				system("cls");
+					if(selectA==1) {
+						cout<<"Kereta tersedia: "<<endl;
+						cout<<pesanTiket.tiketNya.namaKa[2]<<"-"<<pesanTiket.tiketNya.jenisKereta[2]<<endl;
+						cout<<" Tanggal keberangkatan: "<<pesanTiket.tiketNya.tglBrkt[2]<<"-"<<pesanTiket.tiketNya.bulan[1]<<"-"<<pesanTiket.tiketNya.tahun<<endl;
+						cout<<" Waktu Keberangkatan: "<<pesanTiket.waktuPergi[2]<<endl;
+						cout<<endl;
+						
+						cout<<"=================================================="<<endl;
+						
+						cout<<"Kereta tersedia: "<<endl;
+						cout<<pesanTiket.tiketNya.namaKa[2]<<"-"<<pesanTiket.tiketNya.jenisKereta[2]<<endl;
+						cout<<" Tanggal keberangkatan: "<<pesanTiket.tiketNya.tglBrkt[4]<<"-"<<pesanTiket.tiketNya.bulan[1]<<"-"<<pesanTiket.tiketNya.tahun<<endl;
+						cout<<" Waktu Keberangkatan: "<<pesanTiket.waktuPergi[2]<<endl;
+						cout<<endl;
+						
+						cout<<"=================================================="<<endl;
+						
+						cout<<"Pesan Tiket?"<<endl;
+					} else {
+						cout<<"Tidak ada dalam pilihan!"<<endl;
+				 } 
+				 
 				break;
 			case 3 :
+				int selectC;
 				cout<<"Pesan tiket kereta ke: "<<endl;
+				cout<<"1."<<pesanTiket.tujuanPergi[1]<<endl;
+				cout<<"2."<<pesanTiket.tujuanPergi[2]<<endl;
+				cout<<"3."<<pesanTiket.tujuanPergi[3]<<endl;
+				cout<<"=================================================="<<endl;
+				cin>>selectC;
+				system("cls");
+					if(selectC==1) {
+						cout<<"Kereta tersedia: "<<endl;
+						cout<<pesanTiket.tiketNya.namaKa[1]<<"-"<<pesanTiket.tiketNya.jenisKereta[1]<<endl;
+						cout<<" Tanggal keberangkatan: "<<pesanTiket.tiketNya.tglBrkt[2]<<"-"<<pesanTiket.tiketNya.bulan[1]<<"-"<<pesanTiket.tiketNya.tahun<<endl;
+						cout<<" Waktu Keberangkatan: "<<pesanTiket.waktuPergi[1]<<endl;
+						cout<<endl;
+						cout<<"Pesan Tiket?"<<endl;
+					} else if(selectC==2) {
+						cout<<"Kereta tersedia: "<<endl;
+						cout<<pesanTiket.tiketNya.namaKa[2]<<"-"<<pesanTiket.tiketNya.jenisKereta[2]<<endl;
+						cout<<" Tanggal keberangkatan: "<<pesanTiket.tiketNya.tglBrkt[2]<<"-"<<pesanTiket.tiketNya.bulan[1]<<"-"<<pesanTiket.tiketNya.tahun<<endl;
+						cout<<" Waktu Keberangkatan: "<<pesanTiket.waktuPergi[2]<<endl;
+						cout<<endl;
+						cout<<"Pesan Tiket?"<<endl;
+					} else if(selectC==3) {
+						cout<<"Kereta tersedia: "<<endl;
+						cout<<pesanTiket.tiketNya.namaKa[3]<<"-"<<pesanTiket.tiketNya.jenisKereta[3]<<endl;
+						cout<<" Tanggal keberangkatan: "<<pesanTiket.tiketNya.tglBrkt[2]<<"-"<<pesanTiket.tiketNya.bulan[1]<<"-"<<pesanTiket.tiketNya.tahun<<endl;
+						cout<<" Waktu Keberangkatan: "<<pesanTiket.waktuPergi[3]<<endl;
+						cout<<endl;
+						cout<<"=================================================="<<endl;
+						cout<<"Pesan Tiket?"<<endl;
+					}
 				break;
 			case 4 :
+				int selectD;
 				cout<<"Pesan tiket kereta ke: "<<endl;
+				cout<<"1."<<pesanTiket.tujuanPergi[1]<<endl;
+				cout<<"2."<<pesanTiket.tujuanPergi[2]<<endl;
+				cout<<"3."<<pesanTiket.tujuanPergi[3]<<endl;
+				cout<<"=================================================="<<endl;
+				cin>>selectD;
+				system("cls");
+					if(selectD==1) {
+						cout<<"Kereta tersedia: "<<endl;
+						cout<<pesanTiket.tiketNya.namaKa[1]<<"-"<<pesanTiket.tiketNya.jenisKereta[1]<<endl;
+						cout<<" Tanggal keberangkatan: "<<pesanTiket.tiketNya.tglBrkt[2]<<"-"<<pesanTiket.tiketNya.bulan[1]<<"-"<<pesanTiket.tiketNya.tahun<<endl;
+						cout<<" Waktu Keberangkatan: "<<pesanTiket.waktuPergi[1]<<endl;
+						cout<<endl;
+						cout<<"Pesan Tiket?"<<endl;
+					} else if(selectD==2) {
+						cout<<"Kereta tersedia: "<<endl;
+						cout<<pesanTiket.tiketNya.namaKa[2]<<"-"<<pesanTiket.tiketNya.jenisKereta[2]<<endl;
+						cout<<" Tanggal keberangkatan: "<<pesanTiket.tiketNya.tglBrkt[2]<<"-"<<pesanTiket.tiketNya.bulan[1]<<"-"<<pesanTiket.tiketNya.tahun<<endl;
+						cout<<" Waktu Keberangkatan: "<<pesanTiket.waktuPergi[2]<<endl;
+						cout<<endl;
+						cout<<"Pesan Tiket?"<<endl;
+					} else if(selectD==3) {
+						cout<<"Kereta tersedia: "<<endl;
+						cout<<pesanTiket.tiketNya.namaKa[3]<<"-"<<pesanTiket.tiketNya.jenisKereta[3]<<endl;
+						cout<<" Tanggal keberangkatan: "<<pesanTiket.tiketNya.tglBrkt[2]<<"-"<<pesanTiket.tiketNya.bulan[1]<<"-"<<pesanTiket.tiketNya.tahun<<endl;
+						cout<<" Waktu Keberangkatan: "<<pesanTiket.waktuPergi[3]<<endl;
+						cout<<endl;
+						cout<<"=================================================="<<endl;
+						cout<<"Pesan Tiket?"<<endl;
+					}
 				break;
 			default: cout<<"Silahkan pilih sesuai yang ada di daftar!"<<endl;
 			}
 		
-			time();
+			//time();
 			thanks();
 		} else if(menu==2) {
 			for (int i=0; i<5; i++) {
-				cout<<" Nama dan No KA: "<< pesanTiket.tiketNya.namaKa[i] <<endl;
-				cout<<" Asal: Kertapati(KPT)" <<" Tujuan: "<<pesanTiket.tujuanPergi[i]<<endl;
-				cout<<" Keberangkatan: "<<pesanTiket.waktuPergi[i]<<endl;
+				cout<<" Nama KA: "<< pesanTiket.tiketNya.namaKa[i] <<endl;
+				cout<<" Asal: "<<pesanTiket.tujuanPulang[i] <<" Tujuan: "<<pesanTiket.tujuanPergi[i]<<endl;
+				cout<<" Tanggal keberangkatan: "<<pesanTiket.tiketNya.tglBrkt[1]<<"-"<<pesanTiket.tiketNya.bulan[1]<<"-"<<pesanTiket.tiketNya.tahun<<endl;
+				cout<<" Jam Keberangkatan: "<<pesanTiket.waktuPergi[i]<<endl;
 				cout<<"\n";
 			};
 			
 			for (int i=0; i<5; i++) {
-				cout<<" Nama dan No KA: "<< pesanTiket.tiketNya.namaKa[i] <<endl;
-				cout<<" Asal: Kertapati(KPT)" <<" Tujuan: "<<pesanTiket.tujuanPulang[i]<<endl;
-				cout<<" Keberangkatan: "<<pesanTiket.waktuPulang[i]<<endl;
+				cout<<" Nama KA: "<< pesanTiket.tiketNya.namaKa[i] <<endl;
+				cout<<" Asal: "<<pesanTiket.tujuanPergi[i] <<" Tujuan: "<<pesanTiket.tujuanPulang[i]<<endl;
+				cout<<" Tanggal keberangkatan: "<<pesanTiket.tiketNya.tglBrkt[2]<<"-"<<pesanTiket.tiketNya.bulan[1]<<"-"<<pesanTiket.tiketNya.tahun<<endl;
+				cout<<" Jam Keberangkatan: "<<pesanTiket.waktuPergi[i]<<endl;
 				cout<<"\n";
 			};
+			
+			cout<<"\n";
+			cout<<"======================================================\n";
 			
 			int backButton;
 			cout<<"1. Kembali 2. Selesai"<<endl;
@@ -135,7 +250,7 @@ int main() {
 				}
 			
 		} else if(menu==3) {
-			cout<<"Blong"<<endl;
+			cout<<"Belum Kepikiran"<<endl;
 			thanks();
 		} else {
 			cout<<"Masukkan Anda tidak dikenal, tolong pilih sesuai angka 1 - 3 sesuai di daftar!"<<endl;
